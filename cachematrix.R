2556a973c58e529b18ce6984f6953171321e179e
Alexey Dodonov <dodonov@gmail.com>
2015-02-20 12:57:37 +0300

    Solve assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedMatrix <- x
+    cachedResult <- NULL
+    set <- function(newMatrix) {
+        cachedMatrix <<- newMatrix
+        cachedResult <<- NULL
+    }
+    get <- function() cachedMatrix
+    setsolve <- function(solve) cachedResult <<- solve
+    getsolve <- function() cachedResult
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    result <- x$getsolve()
+    if(!is.null(result)) {
+        message("getting cached data")
+        return(result)
+    }
+    matrix <- x$get()
+    result <- try(solve(matrix, ...))
+    x$setsolve(result)
+    
+    result
 }
